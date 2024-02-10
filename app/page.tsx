a6286735374e4d4d53f60b3737ece67304e3587c
Hashan <hemachandra.hashan@gmail.com>
2024-02-10 05:59:11 +0530

    Logic for redirecting a new user

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,17 @@
-/* ====================================================================================================================
+/* ================================================================================================ /
  * Title : Home Page
- * Description :This renders the first welcome page of the app
+ * Description : This renders the first welcome page of the app. This checks if user is signed in or not
  * Author : Hashan
- * Date : February 1st, 2024
- * ====================================================================================================================
- */
+ * Date : February 10th, 2024
+ /* ================================================================================================ */
 
+import { auth } from '@clerk/nextjs'
 import Link from 'next/link'
 
-export default function Home() {
+export default async function Home() {
+  const { userId } = await auth() // Check if user is signed in
+
+  let href = userId ? '/journal' : '/new-user'
   return (
     <div className="w-screen h-screen bg-black flex justify-center items-center text-white">
       <div className="w-full max-w-[600px] mx-auto">
@@ -17,7 +20,7 @@ export default function Home() {
           Daily Emotions, Analyzed: Unravel Your Moods with MoodMap
         </p>
         <div>
-          <Link href="/journal">
+          <Link href={href}>
             <button className="bg-blue-600 px-4 py-2 rounded-lg text-xl">
               get started
             </button>
