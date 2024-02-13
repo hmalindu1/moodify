ae60d55a9ffd09f51835ede1ed16465649b234c6
Hashan <hemachandra.hashan@gmail.com>
2024-02-13 23:57:17 +0530

    comments added

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -5,18 +5,32 @@
  * Date : February 11th, 2024
  /* ================================================================================================ */
 
+import { UserButton } from '@clerk/nextjs'
+
+/**
+ * Create a dashboard layout component.
+ *
+ * @param {{ children: React.ReactNode }} param0 - The children to be rendered within the layout.
+ * @return {React.ReactNode} The main layout structure.
+ */
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  // Return the main layout structure
   return (
     <div className="h-screen w-screen relative">
+      {/* Define the sidebar */}
       <aside className="absolute w-[200px] top-0 left-0 h-full border-r border-black/10">
         Mood
       </aside>
+      {/* Define the content area */}
       <div className="ml-[200px]">
+        {/* Define the header */}
         <header className="h-[60px] border-b border-black/10">
           <div className="w-full h-full px-6 flex items-center justify-end">
-            <UserButton/>
+            {/* Render the UserButton component */}
+            <UserButton />
           </div>
         </header>
+        {/* Render the children components within the content area */}
         <div>{children}</div>
       </div>
     </div>
