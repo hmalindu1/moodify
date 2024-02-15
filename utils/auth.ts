1afbd89137fba01e8e4f731280ffd68ac1dd23f9
Hashan <hemachandra.hashan@gmail.com>
2024-02-15 05:59:24 +0530

    Explanations added

--- utils/auth.ts
+++ utils/auth.ts
@@ -1,12 +1,16 @@
-import { auth } from "@clerk/nextjs"
-import { prisma } from "./db"
+import { auth } from '@clerk/nextjs'
+import { prisma } from './db'
 
 export const getUserByClerkId = async () => {
-    const {userId} = await auth()
+  const { userId } = await auth() // Check if user is signed in
 
-    const user = await prisma.user.findUniqueOrThrow({
-        where: {
-            clerkId: userId
-        }
-    })
+  /** Find user,if user does not exist in database, then throw an error, because at this point if user is not in database, it means
+   * that the user has slipped passed through our new user logic. We need to check that error.
+   */
+
+  const user = await prisma.user.findUniqueOrThrow({
+    where: {
+      clerkId: userId,
+    },
+  })
 }
