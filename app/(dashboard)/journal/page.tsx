000b8cb3a4d9e2ac3226cc7e4f25ce39ff10a2c3
Hashan <hemachandra.hashan@gmail.com>
2024-02-15 06:11:00 +0530

    Getting entries using authenticated user

--- app/(dashboard)/journal/page.tsx
+++ app/(dashboard)/journal/page.tsx
@@ -1,11 +1,23 @@
-import { prisma } from "@/utils/db"
+import { getUserByClerkId } from '@/utils/auth'
+import { prisma } from '@/utils/db'
 
 const getEntries = async () => {
-
+  const user = await getUserByClerkId()
+  const entries = await prisma.journalEntry.findMany({
+    where: {
+      userId: user.id,
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+  })
+  return entries
 }
 
-const JournalPage = () => {
-    return <div>Journal</div>
+const JournalPage = async () => {
+  const entries = await getEntries();
+  console.log('Entries: ', entries);
+  return <div>Journal</div>
 }
 
 export default JournalPage
