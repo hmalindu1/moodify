b635bc026f33ee4d4a2fad4b51bd884376703cf7
Hashan <hemachandra.hashan@gmail.com>
2024-02-18 05:27:41 +0530

    onClick handle function created

--- components/NewEntryCard.tsx
+++ components/NewEntryCard.tsx
@@ -5,10 +5,17 @@
  * Date : February 17th, 2024
  /* ================================================================================================ */
 'use client'
+import { createNewEntry } from '@/utils/api'
+import { useRouter } from 'next/navigation'
 import React from 'react'
 
 const NewEntryCard = () => {
-  const handleOnClick = () => {}
+  const router = useRouter()
+
+  const handleOnClick = async () => {
+    const data = await createNewEntry()
+    router.push(`/journal/${data.id}`)
+  }
   return (
     <div className="cursor-pointer overflow-hidden rounded-lg bg-white shadow">
       <div className="px-4 py-5 sm:p-6" onClick={handleOnClick}>
