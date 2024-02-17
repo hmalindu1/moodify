954d428e184dc8dbf7dab169da51b684aa311d89
Hashan <hemachandra.hashan@gmail.com>
2024-02-17 17:13:57 +0530

    Entry Card component created

---
+++ components/EntryCard.tsx
@@ -0,0 +1,14 @@
+/* ================================================================================================ /
+ * Title : Entry Card Component
+ * Description : 
+ * Author : Hashan
+ * Date : February 17th, 2024
+ /* ================================================================================================ */
+
+import React from 'react'
+
+const EntryCard = ({ entry }) => {
+  return <div>{entry.id}</div>
+}
+
+export default EntryCard
