36a7e14fb9e140381b6d5f3981849190bb26895c
Hashan <hemachandra.hashan@gmail.com>
2024-02-17 18:14:15 +0530

    onClick function for New Entry button

--- components/NewEntryCard.tsx
+++ components/NewEntryCard.tsx
@@ -4,13 +4,14 @@
  * Author : Hashan
  * Date : February 17th, 2024
  /* ================================================================================================ */
-
+'use client'
 import React from 'react'
 
 const NewEntryCard = () => {
+  const handleOnClick = () => {}
   return (
-    <div className='cursor-pointer overflow-hidden rounded-lg bg-white shadow'>
-      <div className="px-4 py-5 sm:p-6">
+    <div className="cursor-pointer overflow-hidden rounded-lg bg-white shadow">
+      <div className="px-4 py-5 sm:p-6" onClick={handleOnClick}>
         <span className="text-3xl">New Entry</span>
       </div>
     </div>
