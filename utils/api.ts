4187dd24d1dccea887299b574230d9791dbde6e9
Hashan <hemachandra.hashan@gmail.com>
2024-02-18 05:26:58 +0530

    Function to call API

--- utils/api.ts
+++ utils/api.ts
@@ -4,3 +4,23 @@
  * Author : Hashan
  * Date : February 17th, 2024
  /* ================================================================================================ */
+
+/***
+ * To pass the full path URLs to the fetch API, this function will create a dynamic url, wether we are on the localhost or on the production
+ */
+export const createURL = (path) => {
+  return window.location.origin + path
+}
+
+export const createNewEntry = async () => {
+  const res = await fetch(
+    new Request(createURL('/api/journal'), {
+      method: 'POST',
+    })
+  )
+
+  if (res.ok) {
+    const data = await res.json()
+    return data.data
+  }
+}
