5fe4ea768137b00ef662d92b2cebcc5aa18c1de1
Hashan <hemachandra.hashan@gmail.com>
2024-02-17 18:14:52 +0530

    Function file to call API

---
+++ utils/api.ts
@@ -0,0 +1,6 @@
+/* ================================================================================================ /
+ * Title : Function to call the Create Journal API
+ * Description : This will get the user through middleware and create a journal 
+ * Author : Hashan
+ * Date : February 17th, 2024
+ /* ================================================================================================ */
