2ee311f9d122f21a32db2fb61322a4701fdf38ac
Hashan <hemachandra.hashan@gmail.com>
2024-02-18 20:27:26 +0530

    Entry Editor Page Added

---
+++ app/(dashboard)/journal/[id]/page.tsx
@@ -0,0 +1,12 @@
+/* ================================================================================================ /
+ * Title : Entry Editor Page
+ * Description : This will get the user through middleware and create a journal 
+ * Author : Hashan
+ * Date : February 17th, 2024
+ /* ================================================================================================ */
+
+const EntryPage = ({ params }) => {
+  return <div>{params.id}</div>
+}
+
+export default EntryPage
