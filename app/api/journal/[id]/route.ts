df5509bca56dbde94c1fb20e854288f5176e52f8
Hashan <hemachandra.hashan@gmail.com>
2024-02-19 05:36:23 +0530

    API for updating the journal entry

---
+++ app/api/journal/[id]/route.ts
@@ -0,0 +1,27 @@
+/* ================================================================================================ /
+ * Title : API for updating the journal
+ * Description : 
+ * Author : Hashan
+ * Date : February 17th, 2024
+ /* ================================================================================================ */
+import { getUserByClerkId } from '@/utils/auth'
+import { prisma } from '@/utils/db'
+import { NextResponse } from 'next/server'
+
+export const PATCH = async (request: Request, { params }) => {
+  const { content } = await request.json()
+  const user = await getUserByClerkId()
+  const updatedEntry = await prisma.journalEntry.update({
+    where: {
+      userId_id: {
+        userId: user.id,
+        id: params.id,
+      },
+    },
+    data: {
+      content,
+    },
+  })
+
+  return NextResponse.json({ data: updatedEntry })
+}
