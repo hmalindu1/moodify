d150506f93f51e9505d29815deb2ebc2b56184f5
Hashan <hemachandra.hashan@gmail.com>
2024-02-25 14:21:02 +0530

    Added entry analysis to UI

--- app/(dashboard)/journal/[id]/page.tsx
+++ app/(dashboard)/journal/[id]/page.tsx
@@ -18,6 +18,9 @@ const getEntry = async (id) => {
         id,
       },
     },
+    include: {
+      analysis: true,
+    },
   })
 
   return entry
@@ -25,11 +28,13 @@ const getEntry = async (id) => {
 
 const EntryPage = async ({ params }) => {
   const entry = await getEntry(params.id)
+  const { mood, summary, color, subject, negative } = entry?.analysis
   const analysisData = [
-    { name: 'Summary', value: '' },
-    { name: 'Subject', value: '' },
-    { name: 'Mood', value: '' },
-    { name: 'Nagative', value: 'False' },
+    { name: 'Summary', value: summary },
+    { name: 'Subject', value: subject },
+    { name: 'Mood', value: mood },
+    { name: 'Nagative', value: negative ? 'Yes' : 'No' },
+    { name: 'Color', value: color },
   ]
   return (
     <div className="h-full w-full grid grid-cols-3">
@@ -37,14 +42,17 @@ const EntryPage = async ({ params }) => {
         <Editor entry={entry} />
       </div>
       <div className="border-l border-black/10">
-        <div className="bg-blue-300 px-6 py-10">
+        <div className="px-6 py-10" style={{ backgroundColor: color }}>
           <h2 className="text-2xl">Analysis</h2>
         </div>
         <div>
           <ul>
             {analysisData.map((item) => (
-              <li key={item.name} className="px-2 py-4 border-b border-t border-black/10 flex items-center justify-between">
-                <span className='text-lg font-semibold'>{item.name}</span>
+              <li
+                key={item.name}
+                className="px-2 py-4 border-b border-t border-black/10 flex items-center justify-between"
+              >
+                <span className="text-lg font-semibold">{item.name}</span>
                 <span>{item.value}</span>
               </li>
             ))}
