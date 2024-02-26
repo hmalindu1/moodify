b9005a7c6422c58669607db3b50f295fc934366d
Hashan <hemachandra.hashan@gmail.com>
2024-02-27 05:25:45 +0530

    Question component created and added to the journal page

---
+++ components/Question.tsx
@@ -0,0 +1,41 @@
+/* ================================================================================================ /
+ * Title : Question Component
+ * Description : 
+ * Author : Hashan
+ * Date : February 27th, 2024
+ /* ================================================================================================ */
+
+'use client'
+
+import { useState } from 'react'
+
+const Question = () => {
+  const [value, setValue] = useState('')
+  const onChange = (e) => {
+    e.preventDefault()
+
+    //TODO
+  }
+
+  return (
+    <div>
+      <form>
+        <input
+          onChange={onChange}
+          value={value}
+          type="text"
+          placeholder="Ask a question"
+          className="border border-black/20 px-4 py-2 text-lg rounded-lg"
+        />
+        <button
+          type="submit"
+          className="bg-blue-400 px-4 py-2 rounded-lg text-lg"
+        >
+          Ask
+        </button>
+      </form>
+    </div>
+  )
+}
+
+export default Question
