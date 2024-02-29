afffa6e9c6501144f2d71614203bd3e1d4ee604f
Hashan <hemachandra.hashan@gmail.com>
2024-02-29 05:55:49 +0530

    Created a page for centiment score

---
+++ app/(dashboard)/history/page.tsx
@@ -0,0 +1,14 @@
+/* ================================================================================================ /
+ * Title : History page
+ * Description : This tells you about history of your mood and set a sentiment score
+ * Author : Hashan
+ * Date : February 29th, 2024
+ /* ================================================================================================ */
+
+import React from 'react'
+
+const History = () => {
+  return <div>History</div>
+}
+
+export default History
