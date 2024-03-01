91ae44d65b6ebf3a6778d2a4873737a3251d02f6
Hashan <hemachandra.hashan@gmail.com>
2024-03-01 05:59:59 +0530

    analysis data fetch functions added

--- app/(dashboard)/history/page.tsx
+++ app/(dashboard)/history/page.tsx
@@ -5,7 +5,18 @@
  * Date : February 29th, 2024
  /* ================================================================================================ */
 
-import React from 'react'
+import { getUserByClerkId } from "@/utils/auth"
+import { prisma } from "@/utils/db"
+
+const getData = async () => {
+  const user = await getUserByClerkId()
+  const analyses =   await prisma.analysis.findMany({
+    where: {
+      userId: user.id
+    }
+  })
+  return analyses
+}
 
 const History = () => {
   return <div>History</div>
