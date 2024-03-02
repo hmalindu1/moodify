98790308ce368824384bcc97f54d3e049fc9596f
Hashan <hemachandra.hashan@gmail.com>
2024-03-02 11:17:09 +0530

    History page completed

--- app/(dashboard)/history/page.tsx
+++ app/(dashboard)/history/page.tsx
@@ -5,21 +5,37 @@
  * Date : February 29th, 2024
  /* ================================================================================================ */
 
-import { getUserByClerkId } from "@/utils/auth"
-import { prisma } from "@/utils/db"
+import HistoryCharts from '@/components/HistoryCharts'
+import { getUserByClerkId } from '@/utils/auth'
+import { prisma } from '@/utils/db'
 
 const getData = async () => {
   const user = await getUserByClerkId()
-  const analyses =   await prisma.analysis.findMany({
+  const analyses = await prisma.analysis.findMany({
     where: {
-      userId: user.id
-    }
+      userId: user.id,
+    },
+    orderBy: {
+      createdAt: 'asc',
+    },
+
   })
-  return analyses
+
+  const sum = analyses.reduce((all, current) => all + current.sentimentScore, 0)
+  const avg = Math.round(sum / analyses.length)
+  return { analyses, avg }
 }
 
-const History = () => {
-  return <div>History</div>
+const History = async () => {
+  const { analyses, avg } = await getData()
+  return (
+    <div className="w-full h-full">
+      <div>{`Avg. Sentiment ${avg}`}</div>
+      <div className="w-full h-full">
+        <HistoryCharts data={analyses} />
+      </div>
+    </div>
+  )
 }
 
 export default History
