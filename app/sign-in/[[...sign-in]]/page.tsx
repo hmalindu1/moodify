26e59746d61978eebcf34b384e6ed889fb6aae6d
Hashan <hemachandra.hashan@gmail.com>
2024-04-12 06:15:15 +0530

    js docs added

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,10 @@
 import { SignIn } from '@clerk/nextjs'
 
+/**
+ * A simple functional component for rendering the SignIn page.
+ *
+ * @return {JSX.Element} The SignIn component
+ */
 const SignInPage = () => {
   return <SignIn />
 }
