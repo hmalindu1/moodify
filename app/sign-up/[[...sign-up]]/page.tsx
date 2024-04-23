0fd98f695e619396ccb7fd3987b9c81071c8bc06
Hashan <hemachandra.hashan@gmail.com>
2024-04-23 23:55:24 +0530

    js docs added

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,10 @@
 import { SignUp } from '@clerk/nextjs'
 
+/**
+ * Renders the SignUpPage component.
+ *
+ * @return {JSX.Element} The rendered SignUpPage component.
+ */
 const SignUpPage = () => {
   return <SignUp afterSignInUrl={'/new-user'} redirectUrl={'/new-user'} />
 }
